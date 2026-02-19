764c757e22dff3e78802df1bea7bd8a881b107a4
loading120 <charlieedie55@gmail.com>
2026-02-19 13:07:29 +0000

    added a try_hack_me folder

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -12,6 +12,21 @@ export default defineConfig({
                     items: [
                         {text: 'Dual Boot',link:'kali/dual-boot/'}
                     ]
+                },
+                {
+                    text:'Try Hack me',
+                    collapsed:false,
+                    items: [
+                        {
+                            text: 'Challenges',
+                            collapsed: true,
+                            items: [
+                                { text: 'Take over', link: '/try_hack_me/challenges/take_over/' }
+                  
+                            ]
+                        }
+
+                    ]
                 }
             ]
         }
---
+++ docs/try_hack_me/challenges/take_over/index.md
@@ -0,0 +1,3 @@
+# Sub-domain enumaration
+
+
